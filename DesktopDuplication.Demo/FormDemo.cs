732994059ad616f1d4124547bdf35341f9d10447
Leo Bottaro <leobottaro@gmail.com>
2019-12-16 18:24:46 -0300

    using opentk to limit the update rate (still large overhead)

--- DesktopDuplication.Demo/FormDemo.cs
+++ DesktopDuplication.Demo/FormDemo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -28,14 +29,58 @@ namespace DesktopDuplication.Demo
             {
                 MessageBox.Show(ex.ToString());
             }
+
             frameEvent += frame_status_update;
         }
 
-
+        Stopwatch sw = new Stopwatch();
         private void FormDemo_Shown(object sender, EventArgs e)
         {
-            Thread t = new Thread(frame_capture_loop);
-            t.Start();
+            Thread tr = new Thread(() =>
+            {
+
+                using (var openTKHandler = new GameWindow(1, 1, OpenTK.Graphics.GraphicsMode.Default, "", GameWindowFlags.FixedWindow))
+                {
+                    sw = new Stopwatch();
+                    sw.Restart();
+                    openTKHandler.RenderFrame += OpenTKHandler_UpdateFrame;
+                    openTKHandler.VSync = VSyncMode.Adaptive;
+                    openTKHandler.Run(60.0, 30.0);
+                }
+            });
+            tr.Start();
+        }
+        private void OpenTKHandler_UpdateFrame(object sender, FrameEventArgs e)
+        {
+            DesktopFrame frame = null;
+            try
+            {
+                frame = desktopDuplicator.GetLatestFrame(false, true);
+            }
+            catch { }
+
+            //if (frame != null)
+            //{
+            //LabelCursor.Location = frame.CursorLocation;
+            //LabelCursor.Visible = frame.CursorVisible;
+            ////Debug.WriteLine("--------------------------------------------------------");
+            //foreach (var moved in frame.MovedRegions)
+            //{
+            //    //Debug.WriteLine(String.Format("Moved: {0} -> {1}", moved.Source, moved.Destination));
+            //    MovedRegion.Location = moved.Destination.Location;
+            //    MovedRegion.Size = moved.Destination.Size;
+            //}
+            //foreach (var updated in frame.UpdatedRegions)
+            //{
+            //    //Debug.WriteLine(String.Format("Updated: {0}", updated.ToString()));
+            //    UpdatedRegion.Location = updated.Location;
+            //    UpdatedRegion.Size = updated.Size;
+            //}
+            //this.BackgroundImage = frame.DesktopImage;
+            //}
+
+            frameEvent?.BeginInvoke(sw.Elapsed.TotalMilliseconds, null, null);
+            sw.Restart();
         }
 
         private delegate void frameEventDelegate(double frameMS);
@@ -53,51 +98,10 @@ namespace DesktopDuplication.Demo
                 return;
             }
 
-            if (chart1.Series[0].Points.Count > 200)
+            if (!stopUpdate && chart1.Series[0].Points.Count > 200)
                 chart1.Series[0].Points.RemoveAt(0);
             chart1.Series[0].Points.Add(Math.Round(1000 / frameMS));
             Text = Math.Round(1000 / frameMS).ToString();
-        }
-
-        private void frame_capture_loop()
-        {
-            Stopwatch sw = new Stopwatch();
-                sw.Restart();
-            while (!stopUpdate)
-            {
-                DesktopFrame frame = null;
-                try
-                {
-                    frame = desktopDuplicator.GetLatestFrame(false, true);
-                }
-                catch
-                {
-                    continue;
-                }
-
-                if (frame != null)
-                {
-                    //LabelCursor.Location = frame.CursorLocation;
-                    //LabelCursor.Visible = frame.CursorVisible;
-                    ////Debug.WriteLine("--------------------------------------------------------");
-                    //foreach (var moved in frame.MovedRegions)
-                    //{
-                    //    //Debug.WriteLine(String.Format("Moved: {0} -> {1}", moved.Source, moved.Destination));
-                    //    MovedRegion.Location = moved.Destination.Location;
-                    //    MovedRegion.Size = moved.Destination.Size;
-                    //}
-                    //foreach (var updated in frame.UpdatedRegions)
-                    //{
-                    //    //Debug.WriteLine(String.Format("Updated: {0}", updated.ToString()));
-                    //    UpdatedRegion.Location = updated.Location;
-                    //    UpdatedRegion.Size = updated.Size;
-                    //}
-                    //this.BackgroundImage = frame.DesktopImage;
-                }
-                sw.Stop();
-                frameEvent?.BeginInvoke(sw.Elapsed.TotalMilliseconds,null,null);
-                sw.Restart();
-            }
         }
 
         private void FormDemo_FormClosing(object sender, FormClosingEventArgs e)
