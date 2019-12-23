9f9080d4f20c6f0afde5bbf321e2455cf2eaa104
leocb <leobottaro@gmail.com>
2019-12-23 15:08:52 -0300

    Rename Project Files Created AmbiLight LED

---
+++ DesktopDuplication.Demo/ConfigForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DesktopDuplication.Demo
+{
+    partial class ConfigForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 200D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisX.Maximum = 200D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Interval = 30D;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.Maximum = 120D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.MinorGrid.Enabled = true;
+            chartArea1.AxisY.MinorGrid.Interval = 10D;
+            chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY.ScaleBreakStyle.Spacing = 1D;
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Bisque;
+            chartArea1.AxisY2.Interval = 10D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(83, 148);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(562, 157);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // FormDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 433);
+            this.Controls.Add(this.chart1);
+            this.DoubleBuffered = true;
+            this.Name = "FormDemo";
+            this.Text = "Desktop Duplication API Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormDemo_FormClosing);
+            this.Shown += new System.EventHandler(this.ConfigForm_Shown);
+            this.ResizeBegin += new System.EventHandler(this.FormDemo_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.FormDemo_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ DesktopDuplication.Demo/ConfigForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopDuplication.Demo
+{
+    public partial class ConfigForm : Form
+    {
+
+        private bool performanceMonitor = true;
+        private DesktopDuplicator desktopDuplicator;
+        Stopwatch sw = new Stopwatch();
+
+        public ConfigForm()
+        {
+            InitializeComponent();
+
+            try
+            {
+                desktopDuplicator = new DesktopDuplicator(0, DesktopDuplicator.VSyncLevel.DoubleVsync);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            if (!performanceMonitor) chart1.Visible = false;
+            if (performanceMonitor) PerformanceMonitorEvent += frame_status_update;
+        }
+
+        private void ConfigForm_Shown(object sender, EventArgs e)
+        {
+            Thread tr = new Thread(() =>
+            {
+                if (performanceMonitor) sw.Restart();
+                captureLoop();
+            });
+            tr.Start();
+        }
+        DesktopFrame frame = null;
+
+        private void captureLoop()
+        { 
+            while (!stopUpdate)
+            {
+                try
+                {
+                    frame = desktopDuplicator.GetLatestFrame();
+                }
+                catch { }
+
+                if (frame == null) continue;
+
+                // uncomment the line bellow to enable drawing on screen
+                // if(updateBgEnabled) BackgroundImage = frame.DesktopImage;
+                
+                PerformanceMonitorEvent?.BeginInvoke(sw.Elapsed.TotalMilliseconds, null, null);
+                if (performanceMonitor) sw.Restart();
+            }
+        }
+
+        private delegate void frameEventDelegate(double frameMS);
+        private static event frameEventDelegate PerformanceMonitorEvent;
+
+        private void frame_status_update(double frameMS)
+        {
+            if (stopUpdate) return;
+            if (InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new frameEventDelegate(frame_status_update), new object[] { frameMS });
+                }
+                catch { }
+                return;
+            }
+
+            if (chart1.Series[0].Points.Count > 200)
+                chart1.Series[0].Points.RemoveAt(0);
+            chart1.Series[0].Points.Add(Math.Round(1000 / frameMS));
+            Text = Math.Round(1000 / frameMS).ToString();
+        }
+
+        private void FormDemo_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            stopUpdate = true;
+        }
+
+        private bool stopUpdate = false;
+        private bool updateBgEnabled = true;
+
+        private void FormDemo_ResizeBegin(object sender, EventArgs e)
+        {
+            BackgroundImage = null;
+            updateBgEnabled = false;
+        }
+
+        private void FormDemo_ResizeEnd(object sender, EventArgs e)
+        {
+            updateBgEnabled = true;
+        }
+    }
+}
